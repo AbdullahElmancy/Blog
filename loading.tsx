7e31526803a6950d6b2644326fd5a4802c945fe9
AbdullahElmancy <aelmancee@gmail.com>
2024-12-12 15:35:35 +0200

    add loading page

---
+++ loading.tsx
@@ -0,0 +1,6 @@
+import LoadingSkeleton from "@/app/components/loadingSkelton/LoadingSkeleton";
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <LoadingSkeleton />
+  }
---
+++ src/app/components/loadingSkelton/LoadingSkeleton.tsx
@@ -0,0 +1,8 @@
+import style from './load.module.css'
+function LoadingSkeleton() {
+  return (
+    <div className={style.load}><h1>Wait Untill Loading</h1></div>
+  )
+}
+
+export default LoadingSkeleton
