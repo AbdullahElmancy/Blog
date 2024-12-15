9a5f204725289461c04129f1eb15abf7013b1d0d
AbdullahElmancy <aelmancee@gmail.com>
2024-12-15 06:55:51 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" type="image/svg+xml" href="/elmancylogo.svg" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
       <AuthProvider>
       <ThemeContextProvider>
